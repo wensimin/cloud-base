56760d6e2dec47e63348666489c74cc2e7ce6740
wensimin <hukaizhiyu2@gmail.com>
2017-09-26 23:18:18 +0800

    启动服务端基本配置

--- src/main/java/tech/shali/project/app/Application.java
+++ src/main/java/tech/shali/project/app/Application.java
@@ -1,11 +1,8 @@
 package tech.shali.project.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  *  启动类
@@ -13,17 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * 		基础配置
  */
-@Configuration
-@EnableAutoConfiguration
-@EnableEurekaClient
-@RestController
+@SpringBootApplication
+@EnableEurekaServer
 public class Application {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
