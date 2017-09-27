5742d4c523f1ba216eb37d9f3c285770013df97d
wensimin <hukaizhiyu2@gmail.com>
2017-09-27 22:42:19 +0800

    cloud - 客户端

--- src/main/java/tech/shali/project/app/Application.java
+++ src/main/java/tech/shali/project/app/Application.java
@@ -3,6 +3,7 @@ package tech.shali.project.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  *  启动类
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableZuulProxy
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
